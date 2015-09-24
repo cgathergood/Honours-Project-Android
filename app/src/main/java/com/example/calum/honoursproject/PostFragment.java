743cc89e2572b8ffc64fa8675bfb7f839d5975ba
Server <calumgathergood@gmail.com>
2015-09-24 17:23:25 +0100

    Removed animation for now. May be implemented again later but its definitely a 'nice to have'

--- app/src/main/java/com/example/calum/honoursproject/PostFragment.java
+++ app/src/main/java/com/example/calum/honoursproject/PostFragment.java
@@ -1,9 +1,11 @@
 package com.example.calum.honoursproject;
 
+import android.content.res.ColorStateList;
 import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,16 +25,11 @@ public class PostFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        final FloatingActionButton fab = (FloatingActionButton) getView().findViewById(R.id.fab);
+        final FloatingActionButton fab = (FloatingActionButton) getActivity().findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                fab.setSelected(!fab.isSelected());
-                fab.setImageResource(fab.isSelected() ? R.drawable.animated_plus : R.drawable.animated_minus);
-                Drawable drawable = fab.getDrawable();
-                if (drawable instanceof Animatable) {
-                    ((Animatable) drawable).start();
-                }
+
             }
         });
     }
