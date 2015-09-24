40b687caf6e0dc31190efe8518129ae5265cf7f0
Server <calumgathergood@gmail.com>
2015-09-24 16:20:10 +0100

    Added FAB to the post fragment. Animated from plus to minus on click

--- app/src/main/java/com/example/calum/honoursproject/PostFragment.java
+++ app/src/main/java/com/example/calum/honoursproject/PostFragment.java
@@ -1,10 +1,15 @@
 package com.example.calum.honoursproject;
 
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class PostFragment extends Fragment {
 
@@ -13,5 +18,22 @@ public class PostFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_post, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final FloatingActionButton fab = (FloatingActionButton) getView().findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fab.setSelected(!fab.isSelected());
+                fab.setImageResource(fab.isSelected() ? R.drawable.animated_plus : R.drawable.animated_minus);
+                Drawable drawable = fab.getDrawable();
+                if (drawable instanceof Animatable) {
+                    ((Animatable) drawable).start();
+                }
+            }
+        });
     }
 }
