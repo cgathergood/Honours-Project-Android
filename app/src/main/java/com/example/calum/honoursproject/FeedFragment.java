805f8ed9f6e118ef3f0b408c5efc3dbfb37e9352
Server <calumgathergood@gmail.com>
2015-10-01 13:24:13 +0100

    Parse request tor receive all posts ordering by newest first

--- app/src/main/java/com/example/calum/honoursproject/FeedFragment.java
+++ app/src/main/java/com/example/calum/honoursproject/FeedFragment.java
@@ -2,17 +2,42 @@ package com.example.calum.honoursproject;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class FeedFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        getPosts();
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_feed, container, false);
+    }
+
+    private void getPosts() {
+        ParseQuery<ParseObject> query = new ParseQuery<>("PhotoTest");
+        query.orderByDescending("createdAt");
+        try {
+            List<ParseObject> posts = query.find();
+            for (ParseObject p : posts) {
+                Log.d("debug", p.getCreatedAt().toString());
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
 }
